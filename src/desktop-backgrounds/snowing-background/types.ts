22e793d61c1282799491efac405f01f9c757ac84
Nirmal <nirmal@lm-kv-nirmal.local>
2024-04-29 11:44:30 +0530

    fix: draw snow flake image instead of sermicircles for snow

--- src/desktop-backgrounds/snowing-background/types.ts
+++ src/desktop-backgrounds/snowing-background/types.ts
@@ -1,4 +1,8 @@
 import { random } from "../../utils/common";
+import SnowFlakeWhite from './snowflake-white.svg';
+
+const image = new Image();
+image.src = SnowFlakeWhite;
 
 export class Particle {
   x;
@@ -21,11 +25,12 @@ export class Particle {
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI, false);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.closePath();
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI, false);
+    // ctx.fillStyle = 'white';
+    // ctx.fill();
+    // ctx.closePath();
+    ctx.drawImage(image, this.x, this.y, this.radius * 2, this.radius * 2)
   }
   update() {
     this.x += this.speedX;
