21bb115aaf7c8daa1bfa619b6ae6c0973036910e
Nirmal <nirmal@lm-kv-nirmal.local>
2024-05-18 20:43:12 +0530

    fix: style the sidebar for todo app

--- src/applications/tod-list/components/sidebar/Sidebar.tsx
+++ src/applications/tod-list/components/sidebar/Sidebar.tsx
@@ -1,15 +1,25 @@
-const Sidebar = () => { 
+type ListItemProps = {
+  name: string;
+};
+const ListItem = ({ name }: ListItemProps) => {
   return (
-    <div className="flex flex-row gap-5">
-      <ul>
-        <li>Folder 1</li>
-        <li>Folder 2</li>
-        <li>Folder 3</li>
-      </ul>
-      <ul>
-        <li>Todo</li>
-        <li>In Progress</li>
-        <li>Done</li>
+    <li className="bg-gray-50/5 p-2 px-4 rounded-lg cursor-pointer">{name}</li>
+  );
+};
+
+const ProjectsList = [
+  { id: 1, name: 'Project 1' },
+  { id: 2, name: 'Project 2' },
+  { id: 3, name: 'Project 3' }
+];
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-row gap-5 min-w-52 bg-black/20 p-5 rounded-lg">
+      <ul className="w-full flex flex-col items-stretch gap-2">
+        {ProjectsList.map((project) => {
+          return <ListItem key={project.id} name={project.name} />;
+        })}
       </ul>
     </div>
   );
